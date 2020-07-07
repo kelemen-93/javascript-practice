3831fbe7f230f87f7a3afeed1ac99d957721c797
Kelemen Gábor <kgabesz18@gmail.com>
2020-07-07 18:32:29 +0200

    bottom right square

---
+++ main4.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('myCanvasFour');
+var context = canvas.getContext('2d');
+
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+
+context.fillRect(canvasWidth / 2, canvasHeight / 2, canvasWidth / 2, canvasHeight / 2);
